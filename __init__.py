ede87c109033fb66385e4a94ab44d4a6017eee53
grotsztaksel <grotsztaksel@o2.pl>
2021-02-22 20:29:22 +0100

    ExpandedTixi: Add __init__.py files

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 22.02.2021 20:18 02
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+"""
+
+__all__ = ['Tixi', 'TixiException', 'ReturnCode']
+__date__ = '2021-02-22'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
+
+from .xtixi import Tixi, TixiException, ReturnCode
---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 22.02.2021 20:21 02
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+"""
+
+__date__ = '2021-02-22'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
